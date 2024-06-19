7f4c541448c073b6697b6cc40ca99e3902349552
lchege <laurachege9@gmail.com>
2024-06-19 05:02:11 +0300

    Main script

--- main.go
+++ main.go
@@ -1,6 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+const conferenceTickets int = 50
+
+var conferenceTickets = "Go conference"
+var remainingTickets uint = 50
+var bookings = make([]UserData, 0)
+
+type UserData struct {
+	firstName string
+	lastName string
+	email    string
+	numberOfTickets uint
+}
+
+var wg = sync.WaitGroup{}
+
 
 func main() {
     fmt.Println("Hello World")
